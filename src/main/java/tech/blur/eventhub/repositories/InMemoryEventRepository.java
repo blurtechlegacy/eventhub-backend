dfe429849f9882aba7b027bafbbaf08763315fa7
Sergey Grechishnikov <35891014+SrgGrch@users.noreply.github.com>
2018-07-13 14:20:40 +0700

    updated date

--- src/main/java/tech/blur/eventhub/repositories/InMemoryEventRepository.java
+++ src/main/java/tech/blur/eventhub/repositories/InMemoryEventRepository.java
@@ -16,12 +16,12 @@ public class InMemoryEventRepository implements EventRepository {
     public InMemoryEventRepository() {
         eventCache.put("1", new Event("1", "2", "SrgGrch", "Two girls one map",
                 "Double math analysis without triple integration", "54.9869693,82.9131314",
-                new ArrayList(Arrays.asList("1","5","2")), new ArrayList(Arrays.asList("1","4","2","6")),"2018-07-03 19:00:00",
-                "2018-07-04 12:00:00"));
+                new ArrayList(Arrays.asList("1","5","2")), new ArrayList(Arrays.asList("1","4","2","6")),"1531440000000",
+                "1531108800000"));
         eventCache.put("2", new Event("2",  "1", "MacOSO", "Mourning",
                 "In memory of good API on JS", "54.8437635,83.0891522",
-                new ArrayList(Arrays.asList("5","2")), new ArrayList(Arrays.asList("1")),"2018-05-03 15:00:00",
-                "2018-06-04 12:00:00"));
+                new ArrayList(Arrays.asList("5","2")), new ArrayList(Arrays.asList("1")),"1531440000000",
+                "1531108800000"));
     }
 
 
