734d6c5c1656d212f71cc2648d741a245f308ab7
Alexandr Sidorenko <MacOSO@users.noreply.github.com>
2018-07-14 01:01:34 +0700

    Add new users

--- src/main/java/tech/blur/eventhub/repositories/InMemoryUserRepository.java
+++ src/main/java/tech/blur/eventhub/repositories/InMemoryUserRepository.java
@@ -23,12 +23,28 @@ public class InMemoryUserRepository implements UserRepository {
                 "1999-09-28", 1));
         userCache.put("3", new User("3", "erbol", "dinara120454", "Ербол",
                 "1999-11-22", 1));
+        userCache.put("4", new User("4", "1", "1", "Матвей",
+                "1999-04-20", 1));
+        userCache.put("5", new User("5", "masanya99", "ilovekonnorrA9", "Маша",
+                "1999-04-10", 0));
+        userCache.put("6", new User("6", "Puzovoz", "pythonmylife", "Илюша",
+                "1999-05-06", 1));
+        userCache.put("7", new User("7", "tipKarpenko", " ", "Илья",
+                "1999-08-24", 1));
         userLoginPass.put(new UserLoginPass("MacOSO", "verystrongpassword"), new User("1", "MacOSO",
                 "verystrongpassword", "Саша", "1999-10-26", 1));
-        userLoginPass.put(new UserLoginPass("SrgGrch", "qwer2017"),new User("2", "SrgGrch",
+        userLoginPass.put(new UserLoginPass("SrgGrch", "qwer2017"), new User("2", "SrgGrch",
                 "qwer2017", "Серёжа","1999-09-28", 1));
-        userLoginPass.put(new UserLoginPass("erbol", "dinara120454"),new User("3", "erbol", "dinara120454", "Ербол",
+        userLoginPass.put(new UserLoginPass("erbol", "dinara120454"), new User("3", "erbol", "dinara120454", "Ербол",
                 "1999-11-22", 1));
+        userLoginPass.put(new UserLoginPass("1", "1"), new User("4", "1", "1", "Матвей",
+                "1999-04-20", 1));
+        userLoginPass.put(new UserLoginPass("masanya99", "ilovekonnorrA9"), new User("5", "masanya99", "ilovekonnorrA9", "Маша",
+                "1999-04-10", 0));
+        userLoginPass.put(new UserLoginPass("Puzovoz", "pythonmylife"), new User("6", "Puzovoz", "pythonmylife", "Илюша",
+                "1999-05-06", 1));
+        userLoginPass.put(new UserLoginPass("tipKarpenko", " "), new User("7", "tipKarpenko", " ", "Илья",
+                "1999-08-24", 1));
     }
 
 
