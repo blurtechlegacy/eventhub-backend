2cbe7a63be7012c92bf8ac51cfba8da78ad38227
Alexandr Sidorenko <MacOSO@users.noreply.github.com>
2018-07-13 23:27:54 +0700

    Adding new tags

--- src/main/java/tech/blur/eventhub/repositories/InMemoryTagRepository.java
+++ src/main/java/tech/blur/eventhub/repositories/InMemoryTagRepository.java
@@ -14,8 +14,19 @@ public class InMemoryTagRepository implements TagRepository {
     private Map<String, Tag> tagCache = new HashMap<>();
 
     public InMemoryTagRepository() {
-        tagCache.put("1", new Tag("1", "mathematics", 0));
-        tagCache.put("2", new Tag("2", "javascript", 0));
+        tagCache.put("1", new Tag("1", "кино", 1));
+        tagCache.put("2", new Tag("2", "природа", 1));
+        tagCache.put("3", new Tag("3", "конференция", 0));
+        tagCache.put("4", new Tag("4", "программирование", 0));
+        tagCache.put("5", new Tag("5", "гитара", 0));
+        tagCache.put("6", new Tag("6", "футбол", 0));
+        tagCache.put("7", new Tag("7", "волейбол", 1));
+        tagCache.put("8", new Tag("8", "скейт", 0));
+        tagCache.put("9", new Tag("9", "велосипед", 0));
+        tagCache.put("10", new Tag("10", "за городом", 0));
+        tagCache.put("12", new Tag("12", "вечеринка", 0));
+        tagCache.put("13", new Tag("13", "парк", 1));
+        tagCache.put("14", new Tag("14", "музей", 0));
     }
 
 
